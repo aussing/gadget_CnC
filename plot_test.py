971b085fd2ed9e1cf26c500839dbddf3de22d174
Adam Ussing <adam.ussing@gmail.com>
2022-08-12 19:27:39 +1000

    plot

---
+++ plot_test.py
@@ -0,0 +1,17 @@
+import pynbody
+import numpy as np
+import matplotlib.pyplot as plt
+
+s = pynbody.load('snapshot_025.hdf5')
+s.physical_units()
+halos = s.halos()
+h1 = halos[1]
+h1_coords = h1['pos']
+
+# pynbody.analysis.halo.center(halos[1], vel=False)
+# fig = pynbody.plot.image(halos[1].d)#, width = '40 kpc', cmap=plt.cm.Greys)#, units = 'Msol kpc^-2')
+# fig = plt.hist2d(h1_coords[:,0],h1_coords[:,1],bins=1000)
+fig = plt.scatter(h1_coords[:,0],h1_coords[:,1],s=0.1)
+fig = plt.xlim((-200,200))
+fig = plt.ylim((-200,200))
+fig = plt.savefig('Pynbody_test.png')
