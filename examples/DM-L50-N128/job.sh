5d7af91b64decdb5a2ead1a911bd436ff0126778
Adam Ussing <adam.ussing@gmail.com>
2022-08-15 15:10:44 +1000

    job update

--- examples/DM-L50-N128/job.sh
+++ examples/DM-L50-N128/job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 ##SBATCH --mail-type=BEGIN,END,FAIL
-#SBATCH --mail-user=harry.potter@swin.edu.au
-#SBATCH --time=15:00:00
+##SBATCH --mail-user=harry.potter@swin.edu.au
+#SBATCH --time=01:30:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=32
 #SBATCH --mem-per-cpu=5000MB
